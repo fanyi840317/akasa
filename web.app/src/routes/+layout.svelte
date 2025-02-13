043a80a93718f1f0449102ab4fb1a2e9d5521c19
fanyi840317 <49425224+fanyi840317@users.noreply.github.com>
2025-02-13 10:47:54 +0800

    loading

--- web.app/src/routes/+layout.svelte
+++ web.app/src/routes/+layout.svelte
@@ -3,12 +3,15 @@
 	import { ModeWatcher } from "mode-watcher";
 	import '../lib/i18n'; // 确保在应用启动时加载 i18n 配置
 	import { waitLocale } from 'svelte-i18n';
+	import Loading from '$lib/components/Loading.svelte';
 	let { children } = $props();
 </script>
 <ModeWatcher />
 
 {#await waitLocale()}
-	<div>Loading...</div>
+	<div class="min-h-screen flex items-center justify-center">
+		<Loading size="lg" />
+	</div>
 {:then}
 {@render children()}
 {/await}
