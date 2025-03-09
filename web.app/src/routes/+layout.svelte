fe833a6f0b0eaabe74aba525fd437181fecd9065
fanyi840317 <49425224+fanyi840317@users.noreply.github.com>
2025-03-09 09:41:19 +0800

    fix Loading path

--- web.app/src/routes/+layout.svelte
+++ web.app/src/routes/+layout.svelte
@@ -3,10 +3,10 @@
 	import { ModeWatcher } from "mode-watcher";
 	import "../lib/i18n"; // 确保在应用启动时加载 i18n 配置
 	import { waitLocale } from "svelte-i18n";
-	import Loading from "$lib/components/Loading.svelte";
+	import Loading from "$lib/components/loading.svelte";
 	let { children } = $props();
-	import Header from "$lib/components/Header.svelte";
-	import Footer from "$lib/components/Footer.svelte";
+	import Header from "$lib/components/header.svelte";
+	import Footer from "$lib/components/footer.svelte";
 	import { Toaster } from "$lib/components/ui/sonner";
 	import { onMount } from "svelte";
 	import { base } from "$app/paths";
