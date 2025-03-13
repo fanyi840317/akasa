ec9cdfd6f7eb407107360c0594c224354f9064dc
fanyi840317 <49425224+fanyi840317@users.noreply.github.com>
2025-03-13 13:07:54 +0800

    fix

--- db/setup_database.js
+++ db/setup_database.js
@@ -6,20 +6,23 @@ const databases = new sdk.Databases(client);
 client
     .setEndpoint('https://cloud.appwrite.io/v1') // Appwrite Cloud API 端点
     .setProject('67ac15990027bfb157f9')
-    .setKey("") // 替换为你的 Appwrite 项目 ID
+    .setKey("standard_122aa6dfe96e880578a6b9857e10dabcddca60f320f38cecd393fdaaedc080f2639432ddab4dcd652c21da0d4b75168f531ab0949a530594d07679df049127ea91f2f22251cd8ca2155aa2847d2c626de7ffbc42f9b0a9b640b534f3daee6eb1f9c3fb46fdb2d84dec6aea8476ef6709b5548e36be9a68ac4f2290925997f96c") // 替换为你的 Appwrite 项目 ID
 
 const DATABASE_ID = '67b344f2000500098ba6'; // 你的数据库 ID
 
 async function setupDatabase() {
     try {
+        
         // 创建集合（cases - 案件）
-        await databases.createCollection(DATABASE_ID, "cases", "案件");
-        await databases.createJsonAttribute(DATABASE_ID, "cases", "title", true);
-        await databases.createJsonAttribute(DATABASE_ID, "cases", "description", false);
-        await databases.createStringAttribute(DATABASE_ID, "cases", "category", 100, true);
-        await databases.createStringAttribute(DATABASE_ID, "cases", "location", 255, false);
-        await databases.createDatetimeAttribute(DATABASE_ID, "cases", "date", true);
-        await databases.createStringAttribute(DATABASE_ID, "cases", "status", 50, true, "open");
+        const collectionId = sdk.ID.unique();
+        await databases.createCollection(DATABASE_ID, collectionId, "cases");
+        await databases.createStringAttribute(DATABASE_ID, collectionId, "title", 255, true);
+        await databases.createStringAttribute(DATABASE_ID, collectionId, "description", 1000, false);
+        await databases.createStringAttribute(DATABASE_ID, collectionId, "content", 36000, false);
+        await databases.createStringAttribute(DATABASE_ID, collectionId, "category", 100, true);
+        await databases.createStringAttribute(DATABASE_ID, collectionId, "location", 255, false);
+        await databases.createDatetimeAttribute(DATABASE_ID, collectionId, "date", true);
+        await databases.createStringAttribute(DATABASE_ID, collectionId, "status", 50, true, "open");
 
         console.log("🎉 数据库初始化完成！");
     } catch (error) {
